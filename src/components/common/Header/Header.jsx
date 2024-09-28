fea7c458776cb4bae851a73e902775d6d0e6e851
Andrew <127788127788a@gmail.com>
2024-09-28 18:26:40 +0200

    creating Header

---
+++ src/components/common/Header/Header.jsx
@@ -0,0 +1,34 @@
+import styles from "./Header.module.css";
+import icon from "./icon.png";
+import cart from "./bag-cross.png";
+import selected from "./heart.png";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <img className={styles.icon} src={icon} alt="Icon" />
+      <nav className={styles.navigation}>
+        <ul>
+          <li>
+            <a href="#">Catalog</a>
+          </li>
+          <li>
+            <a href="#">New collections</a>
+          </li>
+          <li>
+            <a href="#">Best Selling</a>
+          </li>
+          <li>
+            <a href="#">Sale</a>
+          </li>
+        </ul>
+      </nav>
+      <div className={styles.purchases}>
+        <img src={cart} alt="cart" />
+        <img src={selected} alt="selected" />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
