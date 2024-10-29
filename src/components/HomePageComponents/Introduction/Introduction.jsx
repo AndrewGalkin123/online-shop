96b5b795c2f53fd40cf273036d8132995bb882bb
Andrew <127788127788a@gmail.com>
2024-10-29 14:12:06 +0100

    making adaptive web

--- src/components/HomePageComponents/Introduction/Introduction.jsx
+++ src/components/HomePageComponents/Introduction/Introduction.jsx
@@ -1,48 +1,27 @@
 import styles from "./Introduction.module.css";
-import animeFigure from "./AnimeFigure1.jpg";
-import { Link } from "react-router-dom";
-import { useContext } from "react";
-import { PurchasesContext } from "../../../context/PurchasesContext";
+import animeFigure from "./Leviathan1.jpg";
+import ProductCard from "../../../components/common/ProductCard/ProductCard";
 
 const Introduction = () => {
-  const { buyNow } = useContext(PurchasesContext);
-
   return (
     <article className={styles.introduction}>
       <div className={styles.attractionBlock}>
         <p>IMMERSE YOURSELF IN THE WORLD OF JAPANESE CULTURE WITH NEKON</p>
 
-        <button>OUR CATALOG</button>
+        <button className={`${styles.button} ${styles.buttonPrimary}`}>
+          OUR CATALOG
+        </button>
       </div>
-      <div className={styles.card}>
-        <Link to="/forAdults/46">
-          <img src={animeFigure} alt={"Senhime"} />
-        </Link>
-        <div className={styles.purchaseBox}>
-          <div className={styles.cardDescription}>
-            <p className={styles.cardTitle}>Leviathan Yukano</p>
-            <div className={styles.prices}>
-              <p className={styles.actualPrice}>1099¥</p>
-              <s className={styles.oldPrice}>1399¥</s>
-            </div>
-          </div>
-          <button
-            onClick={(e) => {
-              e.stopPropagation();
-              buyNow({
-                id: 46,
-                name: "Leviathan Yukano",
-                price: 1099,
-                originalPrice: 1399,
-                onSale: true,
-                category: "forAdults",
-                imageSrc: animeFigure ? [animeFigure] : [],
-              });
-            }}
-          >
-            BUY NOW
-          </button>
-        </div>
+      <div className={styles.product}>
+        <ProductCard
+          imageSrc={animeFigure}
+          cardTitle="Leviathan Yukano"
+          price={1099}
+          originalPrice={1399}
+          onSale={true}
+          id={46}
+          category="forAdults"
+        />
       </div>
     </article>
   );
