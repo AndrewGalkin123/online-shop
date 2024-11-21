edf28158b07590973d54d28a9d706061015ea342
Andrii Halkin <127788127788a@gmail.com>
2024-11-21 08:51:25 +0100

    some changes

--- src/pages/AboutUs/AboutUsPage.jsx
+++ src/pages/AboutUs/AboutUsPage.jsx
@@ -6,34 +6,33 @@ const AboutUs = () => {
     <div className="about-us">
       <h1>About Us</h1>
       <h2>Welcome to our world of anime figures!</h2>
-      <br></br>
+      <br />
       <p>
-        My name is Andrii Halkin, and I created this project with great passion
-        and enthusiasm. I have always been a fan of anime and manga, and one of
-        the things that inspired me is the amazing figures that bring beloved
-        characters to life and keep them forever in our hearts.
+        My name is Andrii Halkin, and I created this project with a lot of
+        passion and curiosity. As a longtime fan of anime and manga, I have
+        always admired the figures that bring beloved characters to life and
+        capture their essence.
       </p>
       <p>
-        In our store, you will find carefully curated figures that are perfect
-        for collectors as well as those looking to add something special to
-        their decor. We offer figures from the most popular anime series and
-        movies, as well as rare items that are hard to find elsewhere.
+        This project is developed as part of a university course, with the aim
+        of creating a space to study and showcase collectible figures. Here,
+        you’ll find a selection of carefully chosen models that might interest
+        both collectors and anyone looking to add something unique to their
+        surroundings.
       </p>
       <p>
-        We strive to provide our customers not only with quality products but
-        also with excellent service. Each figure undergoes strict quality checks
-        before shipping so that you can be confident you will receive exactly
-        what you expected. Our goal is to make your shopping experience smooth
-        and enjoyable.
+        Our goal is to provide users with high-quality detailed information
+        about each figure. We want to make the process of discovering
+        collectible figures both engaging and informative.
       </p>
       <p>
-        We appreciate your support and hope you find something that inspires you
-        as much as it inspires us. Thank you for visiting our store, and welcome
-        to the world of anime figures!
+        We hope that this project inspires you and brings joy as you explore the
+        world of anime figures. Thank you for your interest in our educational
+        project, and welcome to our world!
       </p>
       <p>
-        If you have any questions or requests, please don't hesitate to reach
-        out. We are always happy to help!
+        If you have any questions or suggestions, please don’t hesitate to reach
+        out. We’re always happy to help!
       </p>
       <p>
         Best regards,
